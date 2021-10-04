85ba6a2b3b16d7c99c544dc0e2d66af3e420ed48
Bhavik Prajapati <rahulprajapati1665@gmail.com>
2021-10-04 13:10:05 +0530

    imageAdded

--- js/app.js
+++ js/app.js
@@ -10,8 +10,7 @@ const rightbtn = document.getElementById("right");
 let img1 = document.getElementById("img1");
 const h1 = document.getElementById("h1");
 const p = document.getElementById("p1");
-document.slide.src =
-  "https://images.unsplash.com/photo-1554744512-5a8ef212982d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+document.slide.src = "../src/hashmi.png";
 
 var imgs = ["../src/hashmi.png", "../src/bhavik.png", "../src/rudra.png"];
 var h1s = ["Hashmi Ranawadia", "Bhavik Prajapati", "rudra Suthar"];
