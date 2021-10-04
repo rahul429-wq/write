ad1d0abf21314ca94ab3e0e9e0e88649fa626f60
Bhavik Prajapati <rahulprajapati1665@gmail.com>
2021-10-04 13:35:17 +0530

    image path set

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const h1 = document.getElementById("h1");
 const p = document.getElementById("p1");
 document.slide.src = "../src/hashmi.png";
 
-var imgs = ["../src/hashmi.png", "../src/bhavik.png", "../src/rudra.png"];
+var imgs = ["src/hashmi.png", "src/bhavik.png", "src/rudra.png"];
 var h1s = ["Hashmi Ranawadia", "Bhavik Prajapati", "rudra Suthar"];
 var ps = ["+9825342173", "+9638271908", "+9825342712"];
 var i = 0;
